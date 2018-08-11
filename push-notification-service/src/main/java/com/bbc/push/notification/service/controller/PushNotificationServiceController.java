1d7e497bbb8df7b31b6533f86f7cf710c43f4d5e
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-11 21:40:17 +0100

    Configured RESTful API for PushNotificationServiceController

--- push-notification-service/src/main/java/com/bbc/push/notification/service/controller/PushNotificationServiceController.java
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/controller/PushNotificationServiceController.java
@@ -1,5 +1,8 @@
 package com.bbc.push.notification.service.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PushNotificationServiceController {
 	
 	
