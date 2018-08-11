f2e3c5d08c1de8009ca57b3123a1f0187fcbf80d
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-11 14:49:15 +0100

    Initialised Spring Boot Application in main class

--- push-notification-service/src/main/java/com/bbc/push/notification/service/push_notification_service/PushNotificationService.java
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/push_notification_service/PushNotificationService.java
@@ -1,12 +1,16 @@
 package com.bbc.push.notification.service.push_notification_service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Push Notification Service
  * 
  * Jonathan Perry
  */
+@SpringBootApplication
 public class PushNotificationService {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(PushNotificationService.class, args);
     }
 }
