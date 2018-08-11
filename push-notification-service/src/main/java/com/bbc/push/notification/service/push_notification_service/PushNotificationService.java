39215d81398fdb1a70162b4d2b35e29a306d96ce
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-11 14:46:39 +0100

    Renamed main class

--- push-notification-service/src/main/java/com/bbc/push/notification/service/push_notification_service/PushNotificationService.java
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/push_notification_service/PushNotificationService.java
@@ -1,11 +1,11 @@
 package com.bbc.push.notification.service.push_notification_service;
 
 /**
- * Hello world!
- *
+ * Push Notification Service
+ * 
+ * Jonathan Perry
  */
-public class App 
-{
+public class PushNotificationService {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
