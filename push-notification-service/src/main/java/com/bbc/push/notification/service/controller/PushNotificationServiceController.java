34b27be1962f05d196674891187f404a98e47edd
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-11 21:50:27 +0100

    Implemented status endpoint

--- push-notification-service/src/main/java/com/bbc/push/notification/service/controller/PushNotificationServiceController.java
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/controller/PushNotificationServiceController.java
@@ -1,10 +1,16 @@
 package com.bbc.push.notification.service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PushNotificationServiceController {
 	
-	
+	@RequestMapping(value = "/",
+		method = RequestMethod.GET)
+	public String status() {
+		return new String("Push Notification Service");
+	}
 
 }
