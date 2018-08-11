a0151fd0577f4517767edc06e0428f40d5163b2f
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-11 14:27:38 +0100

    Initial commit

---
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/push_notification_service/App.java
@@ -0,0 +1,12 @@
+package com.bbc.push.notification.service.push_notification_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
