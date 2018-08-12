3c6005161432576079fb5d6943370cf0f9c09afa
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-12 12:25:13 +0100

    Created user model

---
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/model/User.java
@@ -0,0 +1,28 @@
+package com.bbc.push.notification.service.model;
+
+public class User {
+	
+	private String username;
+	private String accessToken;
+	
+	public User() {
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+}
