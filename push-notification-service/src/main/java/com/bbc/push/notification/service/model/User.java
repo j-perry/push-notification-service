fb2249b433ea4d06465cb3c40c74fa4627becf17
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-13 20:24:49 +0100

    Implemented User model

--- push-notification-service/src/main/java/com/bbc/push/notification/service/model/User.java
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/model/User.java
@@ -1,9 +1,13 @@
 package com.bbc.push.notification.service.model;
 
+import java.util.Date;
+
 public class User {
 	
 	private String username;
 	private String accessToken;
+	private Date creationTime;
+	private int numOfNotificationsPushed;
 	
 	public User() {
 		
@@ -23,6 +27,22 @@ public class User {
 
 	public void setAccessToken(String accessToken) {
 		this.accessToken = accessToken;
+	}
+
+	public Date getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(Date creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public int getNumOfNotificationsPushed() {
+		return numOfNotificationsPushed;
+	}
+
+	public void setNumOfNotificationsPushed(int numOfNotificationsPushed) {
+		this.numOfNotificationsPushed = numOfNotificationsPushed;
 	}
 
 }
