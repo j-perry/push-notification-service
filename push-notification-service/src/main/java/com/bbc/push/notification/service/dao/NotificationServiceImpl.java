3758bc279971eaf600fd3c885d44fca84c9f73da
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-17 18:23:35 +0100

    Implemented business logic creating a new user

--- push-notification-service/src/main/java/com/bbc/push/notification/service/dao/NotificationServiceImpl.java
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/dao/NotificationServiceImpl.java
@@ -1,10 +1,16 @@
 package com.bbc.push.notification.service.dao;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
 import com.bbc.push.notification.service.model.User;
 
+@Service
 public class NotificationServiceImpl implements NotificationService {
 	
 	private ArrayList<User> users;
@@ -14,10 +20,13 @@ public class NotificationServiceImpl implements NotificationService {
 	}
 
 	public User createUser(User user) {
+		LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		
 		User user2 = new User();
 		user2.setUsername(user.getUsername());
 		user2.setAccessToken(user.getAccessToken());
-		user2.setCreationTime(new Date());
+		user2.setCreationTime(now.format(formatter));
 		user2.setNumOfNotificationsPushed(0);
 		
 		this.users.add(user2);
