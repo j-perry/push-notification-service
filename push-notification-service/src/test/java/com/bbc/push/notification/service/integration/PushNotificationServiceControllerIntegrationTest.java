63933bc64acd5a44f9d56903e4a94144e0a9ed0d
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-19 19:10:40 +0100

    Implemented full stack integration test to get all users

--- push-notification-service/src/test/java/com/bbc/push/notification/service/integration/PushNotificationServiceControllerIntegrationTest.java
+++ push-notification-service/src/test/java/com/bbc/push/notification/service/integration/PushNotificationServiceControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.bbc.push.notification.service.integration;
 
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -60,6 +62,48 @@ public class PushNotificationServiceControllerIntegrationTest {
 		assertThat(response.getBody(), is(notNullValue()));
 		assertThat(response.getBody().getAccessToken(), equalTo(accessToken));
 		assertThat(response.getBody().getUsername(), equalTo(username));
+	}
+	
+	@Test
+	public void testGetAllUsers() throws Exception {
+		final int length = 2;
+    	LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        
+        User user1 = new User();
+    	user1.setUsername("Alfie");
+    	user1.setAccessToken("s235svvf7");
+    	user1.setCreationTime(now.format(formatter));
+    	user1.setNumOfNotificationsPushed(0);
+    	
+    	User user2 = new User();
+    	user2.setUsername("Simon");
+    	user2.setAccessToken("td925nn9a");
+    	user2.setCreationTime(now.format(formatter));
+    	user2.setNumOfNotificationsPushed(0);
+    	
+    	HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<User> userOneEntity = new HttpEntity<User>(user1, headers);
+		HttpEntity<User> userTwoEntity = new HttpEntity<User>(user2, headers);
+		
+    	String createUserEndpoint = base.toString() + "/create/user";
+    	ResponseEntity<User> postResponseOne = restTemplate.exchange(createUserEndpoint, HttpMethod.POST, userOneEntity, User.class);
+		assertThat(postResponseOne.getStatusCode(), equalTo(HttpStatus.CREATED));
+
+    	ResponseEntity<User> postResponseTwo = restTemplate.exchange(createUserEndpoint, HttpMethod.POST, userTwoEntity, User.class);
+		assertThat(postResponseTwo.getStatusCode(), equalTo(HttpStatus.CREATED));
+    	
+		headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> allUsersEntity = new HttpEntity<String>("parameters", headers);
+		
+		String getAllUsersEndpoint = base.toString() + "/users/all";
+		ResponseEntity<User[]> response = restTemplate.exchange(getAllUsersEndpoint, HttpMethod.GET, allUsersEntity, User[].class);
+		
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		assertThat(response.getHeaders().getContentType(), equalTo(MediaType.APPLICATION_JSON_UTF8));
+		assertThat(response.getBody(), is(notNullValue()));
+		assertThat(response.getBody().length, equalTo(length));
 	}
 
 }
