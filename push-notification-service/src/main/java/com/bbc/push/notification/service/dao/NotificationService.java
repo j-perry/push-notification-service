5b0077e2666992c6844f638cc763ea1a1be347e9
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-19 18:59:52 +0100

    Implemented get all users service

--- push-notification-service/src/main/java/com/bbc/push/notification/service/dao/NotificationService.java
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/dao/NotificationService.java
@@ -1,9 +1,13 @@
 package com.bbc.push.notification.service.dao;
 
+import java.util.ArrayList;
+
 import com.bbc.push.notification.service.model.User;
 
 public interface NotificationService {
 
 	public User createUser(User user);
+
+	public ArrayList<User> getAllUsers();
 	
 }
