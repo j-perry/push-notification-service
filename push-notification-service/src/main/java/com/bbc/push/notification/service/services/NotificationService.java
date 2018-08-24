cc03ff2cfc025e81ec0150f11404fe6ce2ef2786
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2018-08-24 20:56:40 +0100

    Added exception handling for createPush and dependency to resolve unchecked RuntimeException error

--- push-notification-service/src/main/java/com/bbc/push/notification/service/services/NotificationService.java
+++ push-notification-service/src/main/java/com/bbc/push/notification/service/services/NotificationService.java
@@ -2,6 +2,8 @@ package com.bbc.push.notification.service.services;
 
 import java.util.ArrayList;
 
+import org.springframework.http.ResponseEntity;
+
 import com.bbc.push.notification.service.model.Note;
 import com.bbc.push.notification.service.model.User;
 
@@ -9,7 +11,7 @@ public interface NotificationService {
 
 	public User createUser(User user);
 	public ArrayList<User> getUsers();
-	public User createPush(String username, Note note) throws Exception;
+	public ResponseEntity<User> createPush(String username, Note note) throws Exception;
 	public User findUser(String username);
 	public boolean updateUserNumOfNotificationsPushed(User user);
 	
